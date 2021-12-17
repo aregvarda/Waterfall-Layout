d3e784fd2764fe7d42d97ec346b56f19d4aa2b48
Areg Vardanian <aregv@bk.ru>
2021-12-17 16:00:51 +0300

    add cards to layout

--- WaterfallLayout/ContentView.swift
+++ WaterfallLayout/ContentView.swift
@@ -12,15 +12,15 @@ struct ContentView: View {
         ScrollView {
             HStack(spacing: 16) {
                 VStack {
-                    ForEach(0...3, id: \.self) { index in
-                        CardView()
-                            .frame(height: index % 2 == 0 ? 320 : 200)
+                    ForEach(Array(leftCards.enumerated()), id: \.element) { offset, card in
+                        CardView(card: card)
+                            .frame(height: offset % 2 == 0 ? 320 : 200)
                     }
                 }
                 VStack {
-                    ForEach(0...3, id: \.self) { index in
-                        CardView()
-                            .frame(height: index % 2 != 0 ? 320 : 200)
+                    ForEach(Array(rightCards.enumerated()), id: \.element) { offset, card in
+                        CardView(card: card)
+                            .frame(height: offset % 2 != 0 ? 320 : 200)
                     }
                 }
             }.padding()
@@ -28,7 +28,35 @@ struct ContentView: View {
     }
 }
 
+struct Card: Hashable {
+    let title: String
+    let imageName: String
+}
+
+let leftCards: [Card] = [
+    .init(title: "Kick Boxing",
+          imageName: "kickboxing"),
+    .init(title: "Boxing",
+          imageName: "boxing"),
+    .init(title: "Morning",
+          imageName: "morning"),
+    .init(title: "Fitness",
+          imageName: "fitness")
+]
+
+let rightCards: [Card] = [
+    .init(title: "Pilates",
+          imageName: "morning"),
+    .init(title: "Intervals",
+          imageName: "intervals"),
+    .init(title: "Yoga",
+          imageName: "yoga"),
+    .init(title: "Run",
+          imageName: "boxing")
+]
+
 struct CardView: View {
+    let card: Card
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
     }
